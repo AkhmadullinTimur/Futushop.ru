f686e4300cf5bb7c3ee82e20aee61a1dae341844
AkhmadullinTimur <77680244+AkhmadullinTimur@users.noreply.github.com>
2021-01-25 22:03:15 +0300

    Update script_js.js

--- js/script_js.js
+++ js/script_js.js
@@ -1,6 +1,14 @@
 'use strict'
 
 
+
+
+setTimeout(function(){
+    document.body.style.overflowY = 'scroll';
+    pre_loader.style.opacity = '0';
+    pre_loader.style.zIndex = '-2';
+}, 1200);
+
 let list_of_obj = [];   
 
 btn1.addEventListener('click', function(){
@@ -20,22 +28,54 @@ btn3.addEventListener('click', function(){
 });
 
 
+btn1_2.addEventListener('click', function(){
+    slides_wrapper2.classList.add('show1');
+    slides_wrapper2.classList.remove('show2');
+    slides_wrapper2.classList.remove('show3');
+    slides_wrapper2.classList.remove('show4');
+});
+btn2_2.addEventListener('click', function(){
+    slides_wrapper2.classList.remove('show1');
+    slides_wrapper2.classList.add('show2');
+    slides_wrapper2.classList.remove('show3');
+    slides_wrapper2.classList.remove('show4');
+});
+btn3_2.addEventListener('click', function(){
+    slides_wrapper2.classList.remove('show1');
+    slides_wrapper2.classList.remove('show2');
+    slides_wrapper2.classList.add('show3');
+    slides_wrapper2.classList.remove('show4');
+});
+btn4_2.addEventListener('click', function(){
+    slides_wrapper2.classList.remove('show1');
+    slides_wrapper2.classList.remove('show2');
+    slides_wrapper2.classList.remove('show3');
+    slides_wrapper2.classList.add('show4');
+});
+
+
 window.addEventListener('scroll', function(){
     let dist = window.pageYOffset;
     if (dist >= 600){
       but.style.opacity = '1';
+      but.style.zIndex = '1';
     }
     else{
       but.style.opacity = '0';
+      but.style.zIndex = '-1';
     }
     if (dist >= 90){
         mainnav.style.position = 'fixed';
-        mainnav.style.top = '0px';
-        mainnav.style.borderBottom = '3px solid black';        
+        mainnav.style.top = '0px';     
         mainnav.style.zIndex = '3';
+        let percentage = 0;
+        percentage = dist / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+        indic.style.width = percentage + '%';
+        indic.style.zIndex = '4';
     } else{
         mainnav.style.position = 'relative';
         mainnav.style.borderBottom = '0px solid black';
+        indic.style.width = 0;
     }
 });
 
@@ -52,6 +92,71 @@ but.addEventListener('click', function(){
     }, 1000/75);
 });
 
+about.addEventListener('click', function(){
+    let i = window.pageYOffset;
+    let speed = 0;
+    if (i >= 800){
+        speed = -60;
+    } else{
+        speed = 60;
+    }
+    let timerId = setInterval(function() {
+        i += speed;
+        window.scrollTo(0, i);   
+        if(i >= 800 & speed > 0){
+            clearInterval(timerId);
+            window.scrollTo(0, 800);
+        }
+        if(i <= 800 & speed < 0){
+            clearInterval(timerId);
+            window.scrollTo(0, 800);
+        }
+    }, 1000/75);
+});
+
+news.addEventListener('click', function(){
+    let i = window.pageYOffset;
+    let speed = 0;
+    if (i >= 1940){
+        speed = -60;
+    } else{
+        speed = 60;
+    }
+    let timerId = setInterval(function() {
+        i += speed;
+        window.scrollTo(0, i);   
+        if(i >= 1940 & speed > 0){
+            clearInterval(timerId);
+            window.scrollTo(0, 1940);
+        }
+        if(i <= 1940 & speed < 0){
+            clearInterval(timerId);
+            window.scrollTo(0, 1940);
+        }
+    }, 1000/75);
+});
+
+katal.addEventListener('click', function(){
+    let i = window.pageYOffset;
+    let speed = 0;
+    if (i >= 2700){
+        speed = -60;
+    } else{
+        speed = 60;
+    }
+    let timerId = setInterval(function() {
+        i += speed;
+        window.scrollTo(0, i);   
+        if(i >= 2700 & speed > 0){
+            clearInterval(timerId);
+            window.scrollTo(0, 2700);
+        }
+        if(i <= 2700 & speed < 0){
+            clearInterval(timerId);
+            window.scrollTo(0, 2700);
+        }
+    }, 1000/75);
+});
 
  let flag_dif = 0;
  let member = 0;
@@ -204,7 +309,6 @@ add_fivth_card.addEventListener('click', function(){
             korzina_text.innerHTML = 'Общая стоимость: ' + price + ' рублей.';
         }
     }
-    console.log(list_of_obj.length); 
 });
 add_sixth_card.addEventListener('click', function(){
     if (list_of_obj.length == 0){
@@ -246,3 +350,66 @@ add_sixth_card.addEventListener('click', function(){
       });
       }
     }
+
+
+    let count_1 = 0;
+        let count_1_1 = 0;
+        let count_2 = 0;
+        let count_2_2 = 0;
+        let sign_1 = 4;
+        let sign_1_1 = -1;
+        let sign_2 = -4;
+        let sign_2_2 = 1;
+        function tik_1(){
+            obj_1.style.left = document.body.scrollWidth/2.6 + count_1 + 'px';
+            obj_1.style.top = 400 - count_1_1/4 + 'px';
+            obj_1.style.width = 150 + count_1_1 + 'px';
+            obj_1.style.height = 150 + count_1_1 + 'px';
+            count_1 += sign_1;
+            count_1_1 += sign_1_1;
+            if (count_1 >= 200){sign_1 = -4;
+                obj_1.style.zIndex = '1';
+                obj_2.style.zIndex = '2';}     
+            if (count_1 <= -200){sign_1 = 4;
+                obj_1.style.zIndex = '2';
+                obj_2.style.zIndex = '1';}  
+            if(count_1_1 >= 100){
+                sign_1_1 = -2;
+            }
+            if (count_1_1 <= -100){
+                sign_1_1 = 2;
+            }
+
+        };
+        function tik_2(){
+            obj_2.style.left = document.body.scrollWidth/2.6 + count_2 + 'px';
+            obj_2.style.top = 400 - count_2_2/1.3 + 'px';
+            obj_2.style.width = 150 + count_2_2 + 'px';
+            obj_2.style.height = 150 + count_2_2 + 'px';
+            count_2 += sign_2;
+            count_2_2 += sign_2_2;
+            if (count_2 >= 200){
+                sign_2 = -4;}     
+            if (count_2 <= -200){
+                sign_2 = 4;}  
+            if(count_2_2 >= 100){
+                sign_2_2 = -2;
+            }
+            if (count_2_2 <= -100){
+                sign_2_2 = 2;
+            }
+            
+        };
+        setInterval(function() {
+            tik_1();
+            tik_2();
+        }, 1000/120)
+       
+        let count_of_clicks = 1;
+        theme.addEventListener('click', function(){
+            count_of_clicks++;
+            if(count_of_clicks % 2 == 1)
+                {gif_snow.style.backgroundImage = '';}
+            else
+                {gif_snow.style.backgroundImage = 'url(https://kalejdoskopphotoshopa.ru/wp-content/uploads/2012/12/snow12.gif)';}
+        });
